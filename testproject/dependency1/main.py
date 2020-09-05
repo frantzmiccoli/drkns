fe549c82c8dd1192900f62215d267aaa8481f082
Fräntz Miccoli <frantz.miccoli@fakedomain.com>
2020-09-05 23:15:44 +0200

    Full command line test with specifications verifications

--- testproject/dependency1/main.py
+++ testproject/dependency1/main.py
@@ -4,6 +4,6 @@ date = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
 
 handle = open('/tmp/dependency1.out', 'a')
 
-handle.write(date)
+handle.write(date + "\n")
 
 handle.close()
--- tests/drkns/test_usage.py
+++ tests/drkns/test_usage.py
@@ -1,9 +1,6 @@
 from paver.easy import sh
 
 
-import sys
-
-
 def test_list():
     _clean()
     output = _invoke_drkns('list')
@@ -30,6 +27,46 @@ def test_run_complete():
 
     assert('project1.out' in ls_output)
     assert('project2.out' in ls_output)
+
+
+def test_run_cache():
+    _clean()
+    _invoke_drkns('run')
+    ls_output = sh('ls /tmp', capture=True)
+    assert('project1.out' in ls_output)
+    assert('project2.out' in ls_output)
+
+    sh('rm /tmp/project*.out')
+
+    _invoke_drkns('run')
+    ls_output = sh('ls /tmp', capture=True)
+    # the hash hasn't change the build should not have generated output
+    assert('project1.out' not in ls_output)
+    assert('project2.out' not in ls_output)
+
+    project_file = 'testproject/project1/main.py'
+    sh('echo "# for test" >> ' + project_file, capture=True)
+    _invoke_drkns('run')
+    ls_output = sh('ls /tmp', capture=True)
+    assert('project1.out' in ls_output)
+    assert('project2.out' not in ls_output)
+
+    sh('cat ' + project_file + ' | grep -v test > ' + project_file + '.mod')
+    sh('rm ' + project_file)
+    sh('mv ' + project_file + '.mod ' + project_file)
+
+
+def test_run_no_multi_dependencies_execution():
+    _clean()
+    _invoke_drkns('run')
+
+    dependency_output_file = '/tmp/dependency1.out'
+    line_count = 0
+    with open(dependency_output_file, 'r') as f:
+        for _ in f:
+            line_count += 1
+
+    assert(line_count == 1)
 
 
 def _invoke_drkns(extra_args: str) -> str:
